0e472ff595dcb1595a71e2d942744428798a81be
mrlonely <mrlonely1226@163.com>
2024-10-27 22:22:08 +0800

    feat: 操作系统

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
       output: {
         // 排除某些文件或文件夹
         assetFileNames: (assetInfo) => {
-          if (assetInfo.name.includes('ez_unserialize')) {
+          if (assetInfo.name.includes('.py')) {
             return '';  // 这将导致该文件不被打包
           }
           return assetInfo.name;
