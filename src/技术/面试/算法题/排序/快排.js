617edbb7961e2f650569f7f60db9126beb49ae2c
mrlonely <mrlonely1226@163.com>
2024-12-31 01:50:24 +0800

    feat:排除所有非md文件

---
+++ src/技术/面试/算法题/排序/快排.js
@@ -0,0 +1,29 @@
+
+
+const quickSort = (arr, left=0,right=arr.length-1) => {
+    if(left<right){
+        const pivot = partition(arr, left,right)
+        quickSort(arr, left, pivot-1)
+        quickSort(arr, pivot+1,right)
+    }
+    return arr
+}
+
+const partition = (arr, left, right) =>{
+    const pivot = arr[right]
+    let i = left-1
+
+    for(let j=left;j<right;j++){
+        // 如果当前元素小于基准值
+        if(arr[j]<pivot){
+            i++
+            [arr[i],arr[j]] = [arr[j],arr[i]]
+        }
+    }
+
+    [arr[i+1],arr[right]] = [arr[right],arr[i+1]]
+
+    return i+1
+}
+
+console.log(quickSort([10, 7, 8, 9, 1, 5]))
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'vitepress';
 
 export default defineConfig({
+    // 排除所有非md文件，便于你插一些可以跑的脚本
+    srcExclude: ['**/*.*', '!**/*.md']
 });
