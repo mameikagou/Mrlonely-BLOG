2766582f67611960f65987b68ac85c7526963312
mrlonely <mrlonely1226@163.com>
2025-08-10 00:20:49 +0800

    feat: 面试

--- src/前端/面试/手写题/数组扁平化.js
+++ src/前端/面试/手写题/数组扁平化.js
@@ -1,8 +1,10 @@
 
 // 数组扁平化
 
-const flatten = (arr) =>{
-    while(arr.some(item=>Array.isArray(item))){
+const flatten = (arr, depth=Infinity) =>{
+    let i =0;
+    while(arr.some(item=>i<depth&&Array.isArray(item))){
+        i++;
         arr = [].concat(...arr)
     }
     return arr
@@ -10,13 +12,25 @@ const flatten = (arr) =>{
 
 const arr3 = [1, 2, [3, 4, [5, 6]]];
 
-console.log(flatten(arr3))
+console.log(flatten(arr3, 1))
 
+const flatten2 = (arr, depth=1) => {
 
-const flat = (arr) => {
+    let result = [];
 
-    while(arr.some(item => Array.isArray(item))){
-        arr = [].concat(...arr)
+    const flatten2 = (arr, depth) =>{
+
+        for(const i of arr){
+            if(depth>0&&Array.isArray(i)){
+                flatten2(i,depth-1);
+            }else{
+                result.push(i);
+            }
+        }
     }
-    return arr;
+    flatten2(arr,depth);
+
+    return result;
 }
+
+console.log(flatten2(arr3, 1))
